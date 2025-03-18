2cb399e5c63e6bcd05d2d4e3f00b66904198c9ac
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-03-18 08:49:07 -0300

    feat: create titulos content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Titulos from "./titulos/page";
 
 export default function Home() {
   return (
    <div>
-    <h1>Hello, World!</h1>
+<h1>Página de Títulos</h1>
    </div>
   );
 }
